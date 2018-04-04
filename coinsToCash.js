1b8a18de90150439662b77e65bdbc4e540f57d37
Katheryn Ford <AnEvilHerbivoreLP@gmail.com>
2018-04-04 14:48:21 -0500

    First Commit

---
+++ coinsToCash.js
@@ -0,0 +1,5 @@
+const piggyBank = {quarters:4, nickels:20,dimes:10,pennies:100 };
+
+let dollarAmount = (piggyBank.quarters * .25) + (piggyBank.nickels * .05) + (piggyBank.dimes * .1) + (piggyBank.pennies * .01);
+console.log(dollarAmount);
+;
